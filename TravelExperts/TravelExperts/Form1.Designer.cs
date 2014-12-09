c8a744db039d4f18d95c6351f329c6c762fcb51b
Leisy Moliner <lhmoliner@gmail.com>
2014-12-09 12:39:32 -0700

    form

--- TravelExperts/TravelExperts/Form1.Designer.cs
+++ TravelExperts/TravelExperts/Form1.Designer.cs
@@ -28,12 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btbSubmit = new System.Windows.Forms.Button();
+            this.lblAgentInfo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btbSubmit
+            // 
+            this.btbSubmit.Location = new System.Drawing.Point(145, 411);
+            this.btbSubmit.Name = "btbSubmit";
+            this.btbSubmit.Size = new System.Drawing.Size(139, 38);
+            this.btbSubmit.TabIndex = 0;
+            this.btbSubmit.Text = "Submit";
+            this.btbSubmit.UseVisualStyleBackColor = true;
+            // 
+            // lblAgentInfo
+            // 
+            this.lblAgentInfo.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblAgentInfo.Location = new System.Drawing.Point(753, 43);
+            this.lblAgentInfo.Name = "lblAgentInfo";
+            this.lblAgentInfo.Size = new System.Drawing.Size(287, 75);
+            this.lblAgentInfo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(753, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Agent";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(122, 119);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Package Information";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(75, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Name";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(122, 160);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 22);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 562);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblAgentInfo);
+            this.Controls.Add(this.btbSubmit);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btbSubmit;
+        private System.Windows.Forms.Label lblAgentInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- TravelExperts/TravelExperts/Form1.cs
+++ TravelExperts/TravelExperts/Form1.cs
@@ -16,5 +16,15 @@ namespace TravelExperts
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
