323b27f69607915c36b04002d30643bc53bc7ac5
mbjae <mbjae_camacho@yahoo.com>
2014-12-09 13:27:02 -0700

    Comments
    Added Comments to Product and Package class

--- TravelExperts/TravelExperts/Product.cs
+++ TravelExperts/TravelExperts/Product.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * Class for Products
+ * Created By: John Nguyen
+ * December 9, 2004
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
