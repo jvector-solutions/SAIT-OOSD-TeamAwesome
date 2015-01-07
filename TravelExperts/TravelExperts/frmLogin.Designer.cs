294dff1ac33d5481cf6973b5c9eda7459ca00220
Leisy Moliner <lhmoliner@gmail.com>
2015-01-07 11:33:17 -0700

    login

--- TravelExperts/TravelExperts/frmLogin.Designer.cs
+++ TravelExperts/TravelExperts/frmLogin.Designer.cs
@@ -30,11 +30,17 @@
         {
             this.btnClear = new System.Windows.Forms.Button();
             this.btbSubmit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUserID = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtAgtPassword = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(525, 156);
+            this.btnClear.Location = new System.Drawing.Point(484, 206);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(139, 38);
             this.btnClear.TabIndex = 36;
@@ -43,23 +49,78 @@
             // 
             // btbSubmit
             // 
-            this.btbSubmit.Location = new System.Drawing.Point(378, 156);
+            this.btbSubmit.Location = new System.Drawing.Point(337, 206);
             this.btbSubmit.Name = "btbSubmit";
             this.btbSubmit.Size = new System.Drawing.Size(139, 38);
             this.btbSubmit.TabIndex = 35;
             this.btbSubmit.Text = "Submit";
             this.btbSubmit.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(351, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 37;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(351, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "Agency ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(351, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 39;
+            this.label3.Text = "Password";
+            // 
+            // txtUserID
+            // 
+            this.txtUserID.Location = new System.Drawing.Point(441, 63);
+            this.txtUserID.Name = "txtUserID";
+            this.txtUserID.Size = new System.Drawing.Size(100, 22);
+            this.txtUserID.TabIndex = 40;
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(441, 108);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 22);
+            this.txtA.TabIndex = 41;
+            // 
+            // txtAgtPassword
+            // 
+            this.txtAgtPassword.Location = new System.Drawing.Point(441, 151);
+            this.txtAgtPassword.Name = "txtAgtPassword";
+            this.txtAgtPassword.Size = new System.Drawing.Size(100, 22);
+            this.txtAgtPassword.TabIndex = 42;
+            // 
             // frmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1042, 350);
+            this.ClientSize = new System.Drawing.Size(999, 387);
+            this.Controls.Add(this.txtAgtPassword);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.txtUserID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btbSubmit);
             this.Name = "frmLogin";
             this.Text = "frmLogin";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +128,12 @@
 
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btbSubmit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUserID;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtAgtPassword;
 
     }
 }
