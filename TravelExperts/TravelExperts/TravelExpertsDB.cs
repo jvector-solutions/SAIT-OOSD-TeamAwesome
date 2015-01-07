7a16ea31c17b1e0442594d9e185534fdbdc9916c
mbjae <mbjae_camacho@yahoo.com>
2015-01-07 11:28:05 -0700

    TravelExpertsDB class

--- TravelExperts/TravelExperts/TravelExpertsDB.cs
+++ TravelExperts/TravelExperts/TravelExpertsDB.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TravelExperts
 {
-    class TravelExpertsDB
+    public static class TravelExpertsDB
     {
+        public static SqlConnection GetConnection()
+        {
+            string connectionString =
+                "Data Source=localhost\\sqlexpress;Initial Catalog=TravelExperts;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
     }
 }
