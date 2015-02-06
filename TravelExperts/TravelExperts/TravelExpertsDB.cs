9db355d338b1b2673edd161e83e5fda9a8706a69
mbjae <mbjae_camacho@yahoo.com>
2015-02-06 11:56:11 -0700

    local DB

--- TravelExperts/TravelExperts/TravelExpertsDB.cs
+++ TravelExperts/TravelExperts/TravelExpertsDB.cs
@@ -19,7 +19,8 @@ namespace TravelExperts
         {
             // Connect to the database using a sql server (connected model)
             string connectionString =
-                "Data Source=localhost\\sqlexpress;Initial Catalog=TravelExperts;Integrated Security=True";
+                //"Data Source=localhost\\sqlexpress;Initial Catalog=TravelExperts;Integrated Security=True";
+                "Data Source=(LocalDB)\\v11.0;AttachDbFilename=|DataDirectory|\\TravelExperts.mdf;Integrated Security=True";
             SqlConnection connection = new SqlConnection(connectionString);
             return connection;
         }
